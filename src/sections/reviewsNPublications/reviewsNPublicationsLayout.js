9b4753c3aefbe024241a35e5aa6dde1f4517cc4b
Minoltan <issackpaul95@gmail.com>
2025-11-23 07:57:39 +0530

    New: Publication and review swapped

--- src/sections/reviewsNPublications/reviewsNPublicationsLayout.js
+++ src/sections/reviewsNPublications/reviewsNPublicationsLayout.js
@@ -7,41 +7,42 @@ const ReviewsNPublicationsLayout = () => {
     return (
         <>
             <Grid
-                direction={{xs: 'column-reverse', md: 'row'}}
+                direction={{ xs: 'column-reverse', md: 'row' }}
                 justifyContent='center'
                 alignItems='center'
                 container
                 className="layoutMarginX layoutPaddingTop"
-                mt={{xs: -5, md: 0, lg: 4}}
+                mt={{ xs: -5, md: 0, lg: 4 }}
             >
                 {
                     content.reviews.length > 0 && content.publications.length > 0
-                    ?
-                    <>
-                        <Grid size={{ xs: 12, md: 6 }}>
-                            <ReviewsList />
-                        </Grid>
+                        ?
+                        <>
+                            <Grid size={{ xs: 12, md: 5 }}>
+                                <PublicationsList />
 
-                        <Grid size={{ xs: 12, md: 1 }}></Grid>
+                            </Grid>
 
-                        <Grid size={{ xs: 12, md: 5 }}>
-                            <PublicationsList />
-                        </Grid>
-                    </>
-                    :
-                    content.reviews.length > 0 && content.publications.length === 0
-                    ?
-                    <>
-                        <Grid size={{ xs: 12 }}>
-                            <ReviewsList />
-                        </Grid>
-                    </>
-                    :
-                    <>
-                        <Grid size={{ xs: 12 }}>
-                            <PublicationsList />
-                        </Grid>
-                    </>
+                            <Grid size={{ xs: 12, md: 1 }}></Grid>
+
+                            <Grid size={{ xs: 12, md: 6 }}>
+                                <ReviewsList />
+                            </Grid>
+                        </>
+                        :
+                        content.reviews.length > 0 && content.publications.length === 0
+                            ?
+                            <>
+                                <Grid size={{ xs: 12 }}>
+                                    <ReviewsList />
+                                </Grid>
+                            </>
+                            :
+                            <>
+                                <Grid size={{ xs: 12 }}>
+                                    <PublicationsList />
+                                </Grid>
+                            </>
                 }
             </Grid>
         </>
